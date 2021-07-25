a7333054aae1e2b18e7caa8662e5ea900cb086b4
Aurelien Montmejat <aurelien.montmejat@gmail.com>
2021-07-25 23:07:52 +0200

    Added files dropzone

--- deepcloud/files/views.py
+++ deepcloud/files/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 def add(request):
     if request.method == 'POST':
-        pass
+        print("New file has been dropped:", request.FILES)
+        # logger.info('message')
     
     return render(request, 'files/add.html', {})
     
